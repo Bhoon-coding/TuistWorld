73d0ec4fba87c31b0728548a310ffa700a533973
bhoon-coding <bhooncoding@gmail.com>
2024-08-28 12:58:06 +0900

    Initial Commit

---
+++ TuistWorld/TuistWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuistWorldApp.swift
+//  TuistWorld
+//
+//  Created by BH on 2024/08/28.
+//
+
+import SwiftUI
+
+@main
+struct TuistWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
